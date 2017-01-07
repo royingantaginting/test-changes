2b4512f7c3cfc9b2a31f1f8059c39462c69bf5bf
Roy Inganta Ginting <roy.i.ginting@gdplabs.id>
2017-01-07 15:57:13 +0700

    get path only

--- changes.groovy
+++ changes.groovy
@@ -6,7 +6,11 @@ def getFilesChange(){
 		def entries = changeLogSets[i].items
 		for (int j = 0; j < entries.length; j++) {
 			def entry = entries[j]
-			filesChange.addAll(entry.affectedFiles)
+			def files = new ArrayList(entry.affectedFiles)
+			for (int k = 0; k < files.size(); k++) {
+				def file = files[k]
+				filesChange.add(file.path)
+			}
 		}
 	}
 	return filesChange
