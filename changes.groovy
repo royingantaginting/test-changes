44cfb510abc6d76ed77b8e7a498072c7a6e28534
Roy Inganta Ginting <roy.i.ginting@gdplabs.id>
2017-01-07 15:35:17 +0700

    Fix typo

--- changes.groovy
+++ changes.groovy
@@ -1,21 +1,21 @@
 @NonCPS
-def getChangeFiles() {
-    def changeFiles = []
+def getFilesChange() {
+    def filesChange = []
 
     def changeLogSets = currentBuild.rawBuild.changeSets
 	List items = changeLogSets.getItems()
 	def affectedFiles = items.collect { it.paths }
-	changeFiles = affectedFiles.flatten().findResults
+	filesChange = affectedFiles.flatten().findResults
 	{
        it.path
 	}.sort().unique()
 
-    return changeFiles
+    return filesChange
 }
 
-def getChangeFilesAsString(){
-    def changeFiles = getChangeFiles()
-    return changeFiles.join(" ")
+def getFilesChangeAsString(){
+    def filesChange = getFilesChange()
+    return filesChange.join(" ")
 }
 
 return this
